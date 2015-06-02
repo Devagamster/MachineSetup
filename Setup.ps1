f68f93bdc4a2af8a706b3e1d8c34359897c4b5c8
Keith Simmons <keith@the-simmons.net>
2015-06-02 00:45:25 -0700

    Fixed setup.ps1

--- Setup.ps1
+++ Setup.ps1
@@ -1,30 +1,21 @@
-function Expand-ZIPFile($file, $destination)
-{
-    $shell = new-object -com shell.application
-    $zip = $shell.NameSpace($file)
-    foreach($item in $zip.items())
-    {
-        $shell.Namespace($destination).copyhere($item)
-    }
-}
-
 $HOME = Resolve-Path .
 $env:HOME = $HOME
 $env:Path += ";" + $HOME + "/msysgit"
 $env:Path += ";" + $HOME + "/ConEmu"
 
-(new-object System.Net.WebClient).DownloadFile("https://github.com/Devagamster/MachineSetup/archive/master.zip", "$pwd\MachineSetup.zip")
+$client = new-object System.Net.WebClient
+$MachineSetupRepository = "https://github.com/Devagamster/MachineSetup/raw/master/"
 
-Expand-ZIPFile(".\MachineSetup.zip", ".")
+$client.DownloadFile($MachineSetupRepository + "7-zip/7z.dll", "$pwd\7z.dll")
+$client.DownloadFile($MachineSetupRepository + "7-zip/7z.exe", "$pwd\7z.exe")
 
-(new-object System.Net.WebClient).DownloadFile("https://github.com/msysgit/msysgit/releases/download/Git-1.9.5-preview20150319/PortableGit-1.9.5-preview20150319.7z", "$pwd\msysgit.7z")
+# $client.DownloadFile("https://github.com/msysgit/msysgit/releases/download/Git-1.9.5-preview20150319/PortableGit-1.9.5-preview20150319.7z", "$pwd\msysgit.7z")
+# .\7-zip\7z.exe e .\msysgit.7z.7z -o* -y
 
-.\7-zip\7z.exe e .\msysgit.7z.7z -o* -y
-
-(new-object System.Net.WebClient).DownloadFile("https://github.com/Maximus5/ConEmu/releases/download/v15.05.13/ConEmuPack.150513.7z", "$pwd\ConEmu.7z")
+$client.DownloadFile("https://github.com/Maximus5/ConEmu/releases/download/v15.05.13/ConEmuPack.150513.7z", "$pwd\ConEmu.7z")
 
 .\7-zip\7z.exe e .\ConEmu.7z -o* -y
 
-mv .\ConEmu.xml .\ConEmu
+$client.DownloadFile($MachineSetupRepository + "ConEmu.xml", "$pwd\ConEmu\ConEmu.xml")
 
 .\ConEmu\ConEmuC.exe \AutoAttach
